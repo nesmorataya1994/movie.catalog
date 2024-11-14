b4ae643674e6d9b96944158afa2f61ec6c0a49cd
nmorataya <nestor_morataya@outlook.com>
2024-11-14 09:48:54 -0600

    update script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,7 @@ INSERT INTO roles (name) VALUES ('ADMIN'), ('USER');
 
 INSERT INTO categories (name) VALUES ('ACTION'), ('ADVENTURE'), ('COMEDY'), ('DRAMA'), ('CRIME'), ('ROMANCE'), ('SCIFI'), ('THRILLER');
 
-INSERT INTO users (firstname, lastname, email, password) VALUES ('Admin', 'User', 'admin@example.com','$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW');
+INSERT INTO users (firstname, lastname, email, password) VALUES ('Admin', 'User', 'admin@example.com','$2a$10$tTEFdgztifFRuPCDHYW5Je265oI/n1Tge4SAZoaNhcWOfuS/rY31K');
 
 -- Link the user to the admin role in the junction table
 INSERT INTO users_roles (user_id, role_id) SELECT u.id, r.id FROM users u, roles r WHERE u.email = 'admin@example.com' AND r.name = 'ADMIN';
